3e50d21daa56886a13b0c2cdfb0242bef70e09fb
Pooja <upooja2290@gmail.com>
2021-11-04 22:18:29 -0500

    first commit

---
+++ src/main/java/com/urlscan/urlscan/UrlscanApplication.java
@@ -0,0 +1,14 @@
+package com.urlscan.urlscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlscanApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("hi");
+	}
+
+}
